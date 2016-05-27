82ca5c094d26dcd340c97df510eafbe8deed26ef
Anton Vorobyev <anton@vorobyev.co>
2016-05-27 09:34:14 +0500

    Add continuous integration settings

---
+++ src/index.php
@@ -0,0 +1,37 @@
+<??>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>Stargazer</title>
+
+<body class="page">
+
+<header>
+
+    <div class="title">
+        <h1><a href="#">Stargazer</a></h1>
+        <p class="subheading">Супергерои рядом</p>
+    </div>
+
+</header>
+
+<section id="about" class="banner">
+    <h2>О приложении</h2>
+
+    <p>Наше приложение супергероично!</p>
+</section>
+
+<section id="lead" class="banner">
+    <h2>Через 3,14 недели!</h2>
+
+    <p>Через 3,14 недели здесь будет супергероично.</p>
+</section>
+
+
+</body>
+
+</html>
